a0161f744fbe2686c0985477d9f4b8bb8babe880
rslice <36705656+rslice@users.noreply.github.com>
2021-07-09 12:59:23 -0400

    Delete risk_scores directory
    Removal of unnecessary files

--- risk_scores/__init__.py
+++
@@ -1,5 +0,0 @@
-from .chads2 import chad, Chads2
-from .poaf import poaf, PoafC
-from .afri import afriMale, afriFem, AfriMaleC, AfriFemC
-from .care import care, CareC
-from .npoaf import npoaf, NPoafC
